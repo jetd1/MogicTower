18bce956ab3e79217c09b1b4bc3fde3219238997
Jet Zhang <jetgabr@gmail.com>
2016-12-16 11:47:58 +0800

    写了一点东西 看看提交有没有问题

--- main.cpp
+++ main.cpp
@@ -7,10 +7,30 @@
 
 using namespace std;
 
+/*全局记录塔信息的数据结构*/
+struct
+{
+    /*楼层地图大小和楼层数，当前固定为1 * 13 * 13 */
+    //int height;
+    //int len, wid;
+    MapObj floorMap[MAP_LENGTH][MAP_WIDTH];
+
+}mogicTower;
+
+
 int main()
 {
-    ifstream fin("input.txt");
     
 
     PAUSE;
+}
+
+void readTower()
+{
+    ifstream fin("input.txt");
+    if (!fin)
+    {
+        
+
+    }
 }
