5b5b4ff814b42bea3678c349bfe4780eaf7041cd
mzpyeah <mzpyeah@pku.edu.cn>
2016-12-18 03:19:39 +0800

    主要更改eval.cpp 非常粗糙的实现 待测试

--- eval.cpp
+++ eval.cpp
@@ -1,7 +1,10 @@
 #include "eval.h"
-
-struct door_key
-{	//全局性的钥匙和门
+#include "helpers.h"
+#include "damage.h"
+extern Tower globalMogicTower;
+/*应该没啥用。。因为是静态的，我需要动态的*/
+/*struct door_key	
+{
     int ykey, bkey, rkey;
     int ydoor, bdoor, rdoor;
     door_key()
@@ -29,26 +32,42 @@ struct door_key
                         break;
                 }
     }
-};
+};*/
+
+inline long long cmpMonster(const Status& stat) {	//当前状态玩家与所有怪物交战的伤害总和（不管这个怪物打没打过）
+	auto map = globalMogicTower.mapContent;
+	long long sumDamage = 0;
+	for (int i = 0; i < MAP_LENGTH; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			if (isMonster(map[i][j])) {
+				int temp = getDamage(stat.player, globalMogicTower.monsterInfo[map[i][j]]);
+				if (temp == 999999999)
+					temp = 1000;
+				sumDamage += temp;
+			}
+		}
+	}
+	return sumDamage;
+}
 
 int eval(const Status& stat)
 {
-    int result = 0;
-
-    int blood = stat.player.getHP();
-    int attack = stat.player.getATK();
-    int defend = stat.player.getDEF();
-    int defend_m = stat.player.getMDEF();
-    int blockDelta = stat.head->blockCount;
-    int yellow_key = stat.player.getKeyCount(yellowKey);
-    int blue_key = stat.player.getKeyCount(blueKey);
-    int red_key = stat.player.getKeyCount(redKey);
-    door_key *dk = new door_key();
-    int all;
-    //int yellow_door = stat.
-    /*这部分是剩下的三种门与钥匙的差值，这三个差值也是参数*/
-
-    result += blood;
-
-    return result;
+	int result = 0;
+	/* 指导思想：魔防>物防>物攻>钥匙>HP */
+	int blood = stat.player.getHP();
+	int attack = stat.player.getATK();
+	int defend = stat.player.getDEF();
+	int defend_m = stat.player.getMDEF();
+	int yellow_key = stat.player.getKeyCount(yellowKey);
+	int blue_key = stat.player.getKeyCount(blueKey);
+	int red_key = stat.player.getKeyCount(redKey);
+	//int yellow_door =
+	/*未完成的这部分是剩下的三种门与钥匙的差值，这三个差值也是参数*/
+	/*（如果能动态维护还剩多少门的情况下）*/
+	result -= cmpMonster(stat) * 100 / blood;	//没有测试，现在不知道这个数是不是稳定在一个量级上，再调
+	result += defend_m * 500;
+	result += defend * 300;
+	result += attack * 250;
+	/*暂时写到这，想增加内容为关于钥匙与剩下的门之间的比率换算的权重*/
+	return result;
 }
--- search.cpp
+++ search.cpp
@@ -3,13 +3,14 @@
 #include "search.h"
 #include "trans.h"
 #include "eval.h"
+#include "trans.h"
+#include <vector>
 
+void restore(Status& stat, bool isempty, PlayerInfo backupPlayer, GraphNode* original_head) {
+	stat.player = backupPlayer;
+	stat.head->empty = isempty;
+	stat.head = original_head;
 
-static void restore(Status& stat, bool isempty, PlayerInfo backupPlayer, GraphNode* original_head)
-{
-    stat.player = backupPlayer;
-    stat.head->empty = isempty;
-    stat.head = original_head;
 }
 
 // TODO: Finish This Shit
@@ -18,28 +19,24 @@ int search(Status& stat, int depth, GraphNode* &bestChoice)
     if (depth == MAX_DEPTH)
         return eval(stat);
 
-    PlayerInfo backupPlayer = stat.player;
+	PlayerInfo backupPlayer = stat.player;
     GraphNode* original_head = stat.head;
-    int maxVal = 0;
-    GraphNode *p;
+	int maxVal = 0;
+	GraphNode *p;
 
     auto& next = stat.head->next;
-    for (auto itr = next.begin(); itr != next.end(); ++itr)
-    {
-        bool isempty = (*itr)->empty;
-        if (trans(stat, *itr))
-        {
-            int curVal = search(stat, depth + 1, p);
-            if (maxVal < curVal)
-            {
-                maxVal = curVal;
-                bestChoice = p;
-            }
-            restore(stat, isempty, backupPlayer, original_head);
-        }
-
+	for (auto itr = next.begin(); itr != next.end(); ++itr) {
+		bool isempty = (*itr)->empty;
+		if (trans(stat, *itr)) {
+			int curVal = search(stat, depth + 1, p);
+			if (maxVal < curVal) {
+				maxVal = curVal;
+				bestChoice = p;
+			}
+			restore(stat, isempty, backupPlayer, original_head);
+		}
+        
     }
-
+    
     return maxVal;
 }
-
